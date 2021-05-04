7e2f027f9a0b0d4ffd53c943ec68ba9bf5765599
rayzui <1430527996@qq.com>
2021-05-04 14:33:57 +0800

    find

---
+++ user/find.c
@@ -0,0 +1,69 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+char*
+fmtname(char *path) {
+  char *p;
+  for(p = path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+  return p;
+}
+
+void
+find(char *dir, char *filename) {
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if ((fd = open(dir, 0)) < 0) {
+    fprintf(2, "find: cannot open %s\n", dir);
+    return;
+  }
+
+  if (fstat(fd, &st) < 0) {
+    fprintf(2, "find: cannot stat %s\n", dir);
+    close(fd);
+    return;
+  }
+
+  switch (st.type) {
+  case T_FILE:
+    if (strcmp(fmtname(dir), filename) == 0) {
+      printf("%s\n", dir);
+    }
+    break;
+  case T_DIR:
+    if (strlen(dir) + 1 + DIRSIZ + 1 > sizeof buf) {
+      fprintf(2, "find: path too long\n");
+      break;
+    }
+    strcpy(buf, dir);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+      if (de.inum == 0 || strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      find(buf, filename);
+    }
+    break;
+  }
+  close(fd);
+}
+
+int
+main(int argc, char *argv[]) {
+  if(argc != 3){
+    fprintf(2, "usage: find directory filename\n");
+    exit(1);
+  }
+
+  find(argv[1], argv[2]);
+
+  exit(0);
+}
